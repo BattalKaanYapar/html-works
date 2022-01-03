d9b76d2d1ce6010a63c9c41e7e6af7ab315e1682
Kaan Yapar <93512418+BattalKaanYapar@users.noreply.github.com>
2022-01-03 14:03:23 +0300

    added: homeworks

---
+++ javascript-works/js/odevler.js
@@ -0,0 +1,23 @@
+
+//** 1.Ödev ** 
+for(var i=1;i <20;i++){
+    console.log(i);
+}
+//2.ödev
+for(var i=0;i<=50;i++){
+    if(i % 2 == 0){
+        console.log(i);
+    }
+};
+//deneme ödev 
+for(var i=0;i<60;i++){
+    if(i %2 == 0+1){
+        console.log(i);
+    }
+};
+
+for(var i=0; i<50;i++){
+    if(i % 2 == 1){
+        console.log(i);
+    }
+};
