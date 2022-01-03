b6d5ec1c7f053ba0f84a215e10080837718a4759
Kaan Yapar <93512418+BattalKaanYapar@users.noreply.github.com>
2022-01-03 16:55:38 +0300

    added: activities in dom

---
+++ javascript-works/js/dom-etkinlikleriyle-calismak.js
@@ -0,0 +1,18 @@
+//Dom Etkinlikleriyle Çalışmak
+//https://www.w3schools.com/jsref/dom_ob_event.asp
+//https://developer.mozilla.org/en-US/docs/Web/Events
+
+let greeting = document.querySelector("#greeting")
+greeting.addEventListener("click", domClick)
+
+function domClick(){
+    console.log("Tıklandı")
+    this.style.color == "red" ? this.style.color = "black" : this.style.color = "red" 
+}
+let header = document.querySelector("#header")
+header.addEventListener("mouseover", domOver)
+
+function domOver(){
+    console.log("denetlendi")
+    this.style.color == "red" ? this.style.color = "black" : this.style.color = "red" 
+}
