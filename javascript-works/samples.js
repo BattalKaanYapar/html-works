234b1d0a56ae9dc1aab7296037fb15342e31044f
Kaan Yapar <93512418+BattalKaanYapar@users.noreply.github.com>
2022-01-05 17:44:37 +0300

    styles:changed

---
+++ javascript-works/samples.js
@@ -0,0 +1,31 @@
+//var name = "kaan"
+//var surName = "yapar"
+//document.write(name + surName);
+
+
+var deneme1 = "kaan";
+var deneme2 ="yapar";
+if (deneme1 + deneme2 =="kaanyaparr"){
+    document.write("doğru")
+}else {
+    document.write("Yanlış <br>")
+}
+var fullName = "Battal Kaan Yapar";
+
+
+fullName = "Yarasalar uçuyor mu ?";
+console.log(fullName)
+let password=null;
+console.log(password)
+if (password == null){
+    console.log("BOŞ Bırakılmış")
+}else{
+    console.log("Caz Yapma")
+}
+var sayilar = ["sıfır", "bir", "iki", "üç"];
+delete sayilar[1];
+document.write(sayilar + "<br>" + sayilar.length);
+
+
+var petoph = ["bir","iki","üç"]
+console.log(petoph)
