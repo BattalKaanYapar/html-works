61eb031756a23036b9f2e65be8f1db21d42e1738
Kaan Yapar <93512418+BattalKaanYapar@users.noreply.github.com>
2022-01-06 10:09:06 +0300

    added: break and continue works

--- javascript-works/for-dongusu/ornekler-odevler.js
+++ javascript-works/for-dongusu/ornekler-odevler.js
@@ -1,18 +1,18 @@
-for (var i = 10; i>=0; i--){
-    console.log(i);
-};
-/*Ödev Çift sayıları yazdır. */
-for (var i = 20; i<=50; i++){
-    console.log(i);
-}
-for (var i = 0; i < 50; i++) {
-    if (i % 2 == 0) {
-        console.log(i);
-    };
-};
-/*Ödev tek sayıları yazdır */
-for (var i = 0; i < 50; i++) {
-    if (i % 2 == 1) {
-        console.log(i);
-    };
-};
+// for (var i = 10; i>=0; i--){
+//     console.log(i);
+// };
+// /*Ödev Çift sayıları yazdır. */
+// for (var i = 20; i<=50; i++){
+//     console.log(i);
+// }
+// for (var i = 0; i < 50; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i);
+//     };
+// };
+// /*Ödev tek sayıları yazdır */
+// for (var i = 0; i < 50; i++) {
+//     if (i % 2 == 1) {
+//         console.log(i);
+//     };
+// };
