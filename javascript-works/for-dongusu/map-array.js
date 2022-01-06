30eefca3fdade15a00ecfd096e45fb64e20dc9b9
Kaan Yapar <93512418+BattalKaanYapar@users.noreply.github.com>
2022-01-06 17:52:34 +0300

    added:map array works

--- javascript-works/for-dongusu/map-array.js
+++ javascript-works/for-dongusu/map-array.js
@@ -16,3 +16,25 @@ const yeniArray = sayilar.map(deger => {
 
 console.log(sayilar);
 console.log(yeniArray);
+
+const USERS = ["Ayşe","Mehmet","Tuğçe","Akşın"]
+
+const NEW_USERS = USERS.map(user => user.toLowerCase())
+console.log(NEW_USERS)
+
+// const USERS_OBJ = USERS.map( item =>
+//     {
+//         return{userName: item, shortName: `${item[0]}.`,newName: `${item[0]}.toUpperCase()${item.slice(1)
+//             .toLowerCase()}`}
+//     }
+//   )
+//   console.log(USERS_OBJ)
+
+const USERS_OBJ = USERS.map( item =>(
+
+    {userName: item, shortName: `${item[0]}.`, newName: `${item[0].toUpperCase()}${item.slice(1)
+        .toLowerCase()}`}
+    )
+    
+)
+  console.log(USERS_OBJ)
