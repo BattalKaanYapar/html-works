b44e41750d8c1616434ffc375b94352265c86784
Kaan Yapar <93512418+BattalKaanYapar@users.noreply.github.com>
2022-01-07 15:43:17 +0300

    added:objects methods

---
+++ javascript-works/nesneler/metot-ekleme.js
@@ -0,0 +1,31 @@
+let user1 = {
+    firsName: "Bilgin",
+    lastName: "Uzman",
+    score: [1, 2, 3, 4],
+    isActive :true,
+    shortName: function () {
+        return `${this.firstName} ${this.lastName}`
+    }
+};
+var person = {
+	name: 'Rumeysa',
+	surname: 'Turgut',
+	age: 23,
+	introduce: () => {
+		return `My name is ${this.name} ${this.surname}, I'm ${this.age} yo.`;
+	},
+};
+console.log(person.introduce());
+person.myCity = function () {
+	console.log(`I live in ${this.city}`);
+};
+person.myCity();
+const fonksiyonAdi = () => {console.log("Merhaba Kodluyoruz")}
+console.log(fonksiyonAdi.name);
+// "fonksiyonAdi"
+function Insan(isim,yas) {
+    this.isim = isim;
+    this.yas = yas;
+  }
+const kaan = new Insan("ali", 24);
+console.log(kaan.yas);
--- javascript-works/nesneler/object-key-value.js
+++ javascript-works/nesneler/object-key-value.js
@@ -1,38 +1,38 @@
-let car = {		
-    brand: "BMW", 	// key(anahtar) "brand" "BMW" value(değerini) tutar 
-    year: 2020,  		// key(anahtar) "year" 2020 value(değerini) tutar 
-  };
-  console.log(car.brand);
+// let car = {		
+//     brand: "BMW", 	// key(anahtar) "brand" "BMW" value(değerini) tutar 
+//     year: 2020,  		// key(anahtar) "year" 2020 value(değerini) tutar 
+//   };
+//   console.log(car.brand);
 
-let laptop1 = {
-    brand: "Apple",
-    model: "MacBook Pro",
-    "2kg" : 2,
-    modelName:"Macbook Pro",
-    // model-name: "Macbook Pro",
-    model_name: "Macbook Pro",
-}
-console.log(laptop1);
+// let laptop1 = {
+//     brand: "Apple",
+//     model: "MacBook Pro",
+//     "2kg" : 2,
+//     modelName:"Macbook Pro",
+//     // model-name: "Macbook Pro",
+//     model_name: "Macbook Pro",
+// }
+// console.log(laptop1);
 
-console.log(laptop1.brand)
-console.log(laptop1.model)
-console.log(laptop1["2kg"])
+// console.log(laptop1.brand)
+// console.log(laptop1.model)
+// console.log(laptop1["2kg"])
 
-//Anahtar bilgisine yeni değer eklemek
-laptop1.brand = "Mac"
-laptop1["brand"] = "Mac1"
-console.log(laptop1)
+// //Anahtar bilgisine yeni değer eklemek
+// laptop1.brand = "Mac"
+// laptop1["brand"] = "Mac1"
+// console.log(laptop1)
 
-//Yeni bilgi eklemek
-laptop1.version = "10.15.7"
-console.log(laptop1)
+// //Yeni bilgi eklemek
+// laptop1.version = "10.15.7"
+// console.log(laptop1)
 
-//Anahtar bilgilerine ulaşmak (keys) -->Object.keys(item)
-keys = Object.keys(laptop1)
-console.log(keys)
-console.log(Object.keys(laptop1)); 
+// //Anahtar bilgilerine ulaşmak (keys) -->Object.keys(item)
+// keys = Object.keys(laptop1)
+// console.log(keys)
+// console.log(Object.keys(laptop1)); 
   
-keys.forEach(keyInfo => {
-    console.log(keyInfo);
-    console.log(laptop1[keyInfo])
-});
+// keys.forEach(keyInfo => {
+//     console.log(keyInfo);
+//     console.log(laptop1[keyInfo])
+// });
