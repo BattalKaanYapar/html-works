72609a3808edec3534137752a4733983c79f205e
Kaan Yapar <93512418+BattalKaanYapar@users.noreply.github.com>
2022-01-11 10:51:16 +0300

    styles:changed

--- javascript-works/Sunum/form.js
+++ javascript-works/Sunum/form.js
@@ -1,14 +1,28 @@
-let tutulanSayi = 28;
+//var tuttugumSayi = 28;
+// var kullanici = prompt(
+//   "Aklımdan 1'den 100'e kadar hangi sayıyı tutuyorum, bil bakalım.",
+//   " "
+// );
+// if (tuttugumSayi == kullanici) {
+//   alert("Tebrikler doğru tahmin");
+// } else if (tuttugumSayi > kullanici) {
+//   alert("Üzgünüm. Tuttuğum sayı sizin sayınızdan daha büyüktü.");
+// } else if (tuttugumSayi < kullanici) {
+//   alert("Üzgünüm. Tuttuğum sayı sizin sayınızdan daha küçüktü.");
+// } else {
+//   alert("Üzgünüm. Tuttuğum sayı değil :/");
+// }
+// var tuttugumSayi = 28;
+// var kullaniciSayisi = document.getElementById("#kullaniciSayisi");
 
-function uyari(value){
-   
-    if (tahmin > tutulanSayi){
-        alert("sayınız tutulan sayıdan büyük")
-    }
-    else if (tahmin < tutulanSayi){
-        alert("Sayınız tutulan sayıdan küçük")
-    }
-    else(tahmin === tutulanSayi)
-        alert("Ben daha fazla çalışmak istemiyorum")
-    
-}
+
+// var btnCevap = document.getElementById("uygula");
+// btnCevap.onclick = function () {
+  
+//  if (kullaniciSayisi == tuttugumSayi){
+//      alert("Doğru tahmin")
+//  }else{
+//      alert("yanlış")
+//  }
+// };
+
