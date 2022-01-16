4adca4855d768d21753b3debe97e8fe4617d98cf
Kaan Yapar <93512418+BattalKaanYapar@users.noreply.github.com>
2022-01-17 02:58:28 +0300

    styles:changed

--- javascript-works/javascript-works2/Javascript-works/Javascript-Proje/js/main.js
+++ javascript-works/javascript-works2/Javascript-works/Javascript-Proje/js/main.js
@@ -1,32 +1,9 @@
-let userFormDOM = document.querySelector("#userForm");
-userFormDOM.addEventListener("submit", formHandler);
-const alertDOM = document.querySelector("#alert");
+let userJobsDOM = document.querySelector('#userJobs')
+userJobsDOM.addEventListener('submit', jobsHandler)
 
-const alertFunction = (title, message, className = "warning") => `
-  <div class="alert alert-${className} alert-dismissible fade show" role="alert">
-<strong>${title}</strong>${message}
-<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-</div>
-    `;
-function formHandler(event) {
+function jobsHandler(event){
   event.preventDefault();
-  const USER_NAME = document.querySelector("#username");
-  if (USER_NAME.value) {
-    addItem(USER_NAME.value);
-    USER_NAME.value = "";
-  } else {
-    alertDOM.innerHTML = alertFunction(
-      "Başlık Bilgisi ",
-      "Eksik Bilgi Girdiniz",
-      "primary"
-    );
-  }
+  console.log("İşlem gerçekleşti")
+  const USER_JOBS = document.querySelector('#username')
+  addItem(USER_JOBS.value)
 }
-let userListDOM = document.querySelector("#userList");
-const addItem = (userName) => {
-  let liDOM = document.createElement("li");
-  liDOM.innerHTML = `
-    ${userName}`;
-    userListDOM.append(liDOM);
-};
-
