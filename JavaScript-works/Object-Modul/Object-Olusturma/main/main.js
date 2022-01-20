9b20a25b0e589e0198297d8245530d42611e6195
Kaan Yapar <93512418+BattalKaanYapar@users.noreply.github.com>
2022-01-20 10:05:55 +0300

    styles:changed

--- JavaScript-works/Object-Modul/Object-Olusturma/main/main.js
+++ JavaScript-works/Object-Modul/Object-Olusturma/main/main.js
@@ -18,3 +18,26 @@ let item3 = {}
 let item4 = new Object();
 
 console.log("object1",object1)
+
+var numbers =[45,100,1,8,9,10];
+
+    Show('result',numbers);
+    Show('result2',MaxArray(numbers));
+
+    // function sortArray() {
+    //     numbers.sort(function(x,y) {
+    //         return y-x;
+    //     })
+    // }
+    // Show('result2',numbers[0]);
+//}
+
+function MaxArray(take) {
+    return Math.max.apply(null,take);
+}
+function MinArray(take) {
+    return Math.min.apply(null,take);
+}
+function Show(par,par2) {
+    document.getElementById(par).innerHTML=par2;
+}
--- JavaScript-works/Odev-Serisi/js/ikinci-odev.js
+++ JavaScript-works/Odev-Serisi/js/ikinci-odev.js
@@ -1,32 +1,33 @@
-                           /** 2.Odev Kurallari **/
+/** 2.Odev Kurallari **/
 // html form yapılacak bootstrap kullanılabilir.
-// form ismi = calisanlar olacak 
+// form ismi = calisanlar olacak
 // formda isim, soyisim,tcno,maaslar olacak
 // form gönderildiğinde assağıda ki bir tabloda gösterilecek
-// tüm çalışanlar employess isminde bir array içerisinde tutulucak 
+// tüm çalışanlar employess isminde bir array içerisinde tutulucak
 // array içerisinde çalışana ait object olacak // her çalışanın kendisi içine giricek
 
 // or :
 // employess =[ {
 //    name , surname,citizenshipNumber,maas="",
 //}]
- //tablonun her satırında sil butonu olacak sil ' e basıldığında tablodan silinecek//
-var employess =["kaan yapar","berkay özen","fahreddin yapar","havva yapar"]
-var firstName =["Kaan","Berkay","Fahreddin","Havva"]
-var lastName = ["Yapar","Özen","Yapar","Yapar"]
-var identification =["5875985958","4875985213","9876575958","7421985958"]
-var wage =  [4000,6000,12000,4000]
-var cikti={}
-for(var i=0; i<employess.length; i++){
-    cikti[employess[i]]={
-        firstname:firstName[i],
-        lastname:lastName[i],
-        identification:identification[i],
-        wage:wage[i]
-
-    }
+//tablonun her satırında sil butonu olacak sil ' e basıldığında tablodan silinecek//
+var employess = ["Kaan Yapar", "Berkay Özen", "Fahreddin Yapar", "Havva Yapar"];
+var firstName = ["Kaan", "Berkay", "Fahreddin", "Havva"];
+var lastName = ["Yapar", "Özen", "Yapar", "Yapar"];
+var identification = ["5875985958", "4875985213", "9876575958", "7421985958"];
+var wage = [4000, 6000, 12000, 4000];
+var formDOM = document.getElementById("calisanlar");
+var text = "<th>";
+var textTD = "<td>";
+var textTR = "<tr>";
+var i = 0;
+console.log(formDOM);
+for (i = 0; i < employess.length; i++) {
+  text += employess[i] + " ";
 }
-console.log(cikti);
+text += "</th>";
+for (i = 0; i < identification.length; i++) {
+  textTD += identification[i] + " ";
+}
 
-cikti = document.querySelector("#calisanlar");
-
+document.getElementById("samples").innerHTML = text;
