b7809d7f0addc548e295c9d20dfef64e5fd73317
Kaan Yapar <93512418+BattalKaanYapar@users.noreply.github.com>
2022-01-25 11:12:54 +0300

    styles:changed

--- JavaScript-works/Odev-Serisi/js/ikinci-odev.js
+++ JavaScript-works/Odev-Serisi/js/ikinci-odev.js
@@ -19,7 +19,7 @@ const addItem = ()=>{
     trDOM.appendChild(tdDOM)
   })
   let closeButton= document.createElement('td')
-  closeButton.innerHTML='<button type="button" onClick="removeItem()">Sil</button>' 
+  closeButton.innerHTML='<button type="button" class="btn btn-danger" onClick="removeItem()">Sil</button>' 
   trDOM.appendChild(closeButton);
   employessDOM.appendChild(trDOM);
   
@@ -49,3 +49,76 @@ const removeItem =(indexnum)=>{
 
 
 //let deleteItem = document.createTextNode("X");
+
+
+
+// var myNodelist = document.getElementsByTagName("li");
+// var index;
+// for (index = 0; index < myNodelist.length; index++) {
+//   var span = document.createElement("span");
+//   var txt = document.createTextNode("\u00D7");
+//   span.className = "close";
+//   span.appendChild(txt);
+//   myNodelist[index].appendChild(span);
+// }
+
+
+// var close = document.getElementsByClassName("close");
+// var index;
+// for (index = 0; index < close.length; index++) {
+//   close[index].onclick = function() {
+//     var div = this.parentElement;
+//     div.style.display = "none";
+//   }
+// }
+
+
+
+// for (index = 0; index < myNodelist.length; index++) {
+//     localStorage.setItem(`eleman ${index+1}`,myNodelist[index].innerText)
+//   }
+
+
+
+// var list = document.querySelector('ul');
+// list.addEventListener('click', function(ev) {
+//   if (ev.target.tagName === 'LI') {
+//     ev.target.classList.toggle('checked');
+//   }
+// }, false);
+
+
+// function newElement() {
+//   var li = document.createElement("li");
+//   var inputValue = document.getElementById("task").value;
+//   var t = document.createTextNode(inputValue);
+//   li.appendChild(t);
+//   if (inputValue === '') {
+//     alert("Bir şeyler yazmalısın!");
+//   } else {
+//     document.getElementById("list").appendChild(li);
+//   }
+//   document.getElementById("task").value = "";
+
+ 
+
+//   var span = document.createElement("span");
+//   var txt = document.createTextNode("\u00D7");
+//   span.className = "close";
+//   span.appendChild(txt);
+//   li.appendChild(span);
+//   $("#liveToast").toast("show");
+
+
+  
+//   for (index = 0; index < close.length; index++) {
+//     close[index].onclick = function() {
+//       var div = this.parentElement;
+//       div.style.display = "none";
+//     }
+//   }
+
+
+//   localStorage.setItem(`eleman ${localStorage.length+1}`,li.innerText)
+
+// }
