0bba01dc30d45851bfaa6ee4b60588b47219a0bf
Kaan Yapar <93512418+BattalKaanYapar@users.noreply.github.com>
2022-01-27 10:51:32 +0300

    styles:updated

--- .vscode/Javascript-entries/1-Ic-ice-for-dongusu/main.js
+++ .vscode/Javascript-entries/1-Ic-ice-for-dongusu/main.js
@@ -1,8 +1,5 @@
+let userList = ["Kaan", "Hasan", "Poyray", "Arat", "Kebap"];
 
-let userList=["Kaan","Hasan","Poyray","Arat","Kebap"]
-
-
-for(var i=0;i<userList.lenght;i++){
-    console.log("i<userList.lenght")
-    
+for (var i = 0; i < userList.length; i++) {
+  console.log(userList[i]);
 }
