467cb9c9e3b17a2b76c2296e7928cce4c3c00e9b
Kaan Yapar <93512418+BattalKaanYapar@users.noreply.github.com>
2022-01-27 16:17:45 +0300

    added:new works

--- .vscode/Javascript-entries/For-dongusu/Ic-ice-for/ic-ice.js
+++ .vscode/Javascript-entries/For-dongusu/Ic-ice-for/ic-ice.js
@@ -1,5 +1,9 @@
 let userList = ["Kaan", "Hasan", "Poyray", "Arat", "Kebap"];
 
 for (var i = 0; i < userList.length; i++) {
-  console.log(userList[i]);
+  
+  for(var k =1;k<5;k++){
+    console.log(" <br> " + k);
+  }
+  console.log(" " + userList[i])
 }
---
+++ .vscode/Javascript-entries/For-dongusu/Ic-ice-for/orneklerim.js
@@ -0,0 +1,25 @@
+var i =0;
+var j= 0;
+
+for(i;i<10;i++){
+   for(j;i==j;j++){
+
+   }
+   console.log(i + "-" + j);
+   if(i == 2){
+       console.log("burada break kullandık")
+       break;
+   }
+}
+
+
+var a = 0 ;
+var b = 0;
+for (a ; a<10 ;i++){
+ for (j ; a == j ;j++){
+}
+  console.log(i+'-'+j);
+  if(a == 2){
+     continue;
+   }
+}
