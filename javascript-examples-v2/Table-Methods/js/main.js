079f6422dc84fa218f70e6d8b2265a1312fec65e
Kaan Yapar <93512418+BattalKaanYapar@users.noreply.github.com>
2022-01-28 17:15:28 +0300

    styles:changed

--- javascript-examples-v2/Table-Methods/js/main.js
+++ javascript-examples-v2/Table-Methods/js/main.js
@@ -14,3 +14,13 @@ for(var i=0;i<employess.length;i++){
     console.log(employess[i])
 }
 
+let employess2=["Hanife","Havva","Mahi","Dudu"]
+
+let toplam=employess.concat(employess2);
+
+console.log(toplam);
+
+toplam.reverse();
+console.log(toplam);
+
+console.log(toplam.join());
