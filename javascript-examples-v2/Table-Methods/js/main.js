343b7fa9be815b990968dc6b572539c7bc81cae1
Kaan Yapar <93512418+BattalKaanYapar@users.noreply.github.com>
2022-01-28 16:42:55 +0300

    styles:uphdated

---
+++ javascript-examples-v2/Table-Methods/js/main.js
@@ -0,0 +1,16 @@
+let employess=["Kaan","Hasan","Berkay","Oğuzhan","Yaren"]
+
+console.log(employess)
+
+let oneTable =document.getElementById('oneTable')
+
+console.log(oneTable)
+
+let gonderDOM =document.getElementById('gonder')
+
+
+
+for(var i=0;i<employess.length;i++){
+    console.log(employess[i])
+}
+
