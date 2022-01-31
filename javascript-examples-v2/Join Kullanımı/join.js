74dcc343f5584b598763d835581748e55c18c4d1
Kaan Yapar <93512418+BattalKaanYapar@users.noreply.github.com>
2022-01-31 10:16:15 +0300

    styles:changed

---
+++ javascript-examples-v2/Join Kullanımı/join.js
@@ -0,0 +1,11 @@
+var dizi = ["kaan","hanife","fahrettin","havva",24,29,32,47]
+
+document.write(dizi + "<br><br>")
+
+var diziJoinKullanımı = dizi.join(" - ");
+
+document.write(diziJoinKullanımı + "<br> <br>")
+
+var diziJoinKullanımı2 = dizi.join(" () ");
+
+document.write(diziJoinKullanımı2 + "<br> <br>")
---
+++ javascript-examples-v2/Keys Kullanımı/keys.js
@@ -0,0 +1,15 @@
+let list = ["apple",142,"banana",15,"book",13]
+
+document.write(list + "<br><br>");
+
+var number;
+
+var key = list.keys();
+
+for (number of key){
+    document.write(number + "<br><br>");
+}
+
+list[0] = "Karpuz";
+
+document.write(list + "<br><br>");
