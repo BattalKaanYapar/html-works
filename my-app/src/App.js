f748db7ffc61ab39315604efa636b0ce7ff79562
Kaan Yapar <93512418+BattalKaanYapar@users.noreply.github.com>
2022-02-07 15:17:39 +0300

    styles:changed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,6 +10,7 @@ import Home from './pages/home';
 import Contact from './pages/contact';
 import User from './pages/user';
 import Home2 from './pages/home2';
+import TodoList from './components/TodoList';
 const App=()=> {
   return (
     <div  className='container'>
@@ -19,12 +20,14 @@ const App=()=> {
         <li><Link to="/">Home </Link></li>
         <li><Link to="/contact">Contact </Link></li>
         <li><Link to="/home2">Home2 </Link></li>
+        <li><Link to="/BağlantıAdresi">TodoList</Link></li>
       </ul>
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/contact' element={<Contact/>} />
           <Route path='/user/:id' element={<User/>} />
           <Route path='/home2' element={<Home2/>} />
+          <Route path='/TodoList' element={<TodoList/>}/>
 
         </Routes>
 
--- my-app/src/components/TodoList.js
+++ my-app/src/components/TodoList.js
@@ -2,13 +2,15 @@ import React from 'react';
 const TodoList = ({todoList,task}) => {
   const filterTodoList = todoList.filter((value) => value.toLowerCase().includes(task.toLowerCase()))
   console.log(filterTodoList)
-  return 
+  return (
   <div>
     <ul>
       {filterTodoList.map((item,index)=>(
         <li key={index}>{item}</li>
       ))}
     </ul>
-  </div>;
+  </div>
+  )
+  
 }
 export default TodoList;
